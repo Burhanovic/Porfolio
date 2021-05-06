9c46b15d169d6ed259feff6c666d088bb780bfac
Burhanovic <burhanaljoundi@gmail.com>
2021-05-06 09:46:05 +0200

    first commit

---
+++ src/resumeData.js
@@ -0,0 +1,84 @@
+let resumeData = {
+  "imagebaseurl":"https://techonoeticsclub.github.io/",
+  "name": "Burhan Aljoundi",
+  "role": "Frontend Developer",
+  "linkedinId":"Your LinkedIn Id",
+  "skypeid": "Your skypeid",
+  "roleDescription": "I like dabbling in various parts of frontend development and like to learn about new technologies, write technical articles or simply play games in my free time.",
+  "socialLinks":[
+      {
+        "name":"linkedin",
+        "url":"https://www.linkedin.com/",
+        "className":"fa fa-linkedin"
+      },
+      {
+        "name":"github",
+        "url":"https://github.com/Burhanovic/",
+        "className":"fa fa-github"
+      },
+      {
+        "name":"skype",
+        "url":"http://skype.com",
+        "className":"fa fa-skype"
+      }
+    ],
+  "aboutme":"I have started my career in web development and i am planning to be good at web development",
+  "address":"Germany-Wittenberge",
+  
+  "education":[
+    {
+      "UniversityName":"DCI Berlin",
+      "specialization":"Web Development",
+      "MonthOfPassing":"sep",
+      "YearOfPassing":"2020",
+      "Achievements":"not yet"
+    },
+    
+  ],
+  "work":[
+    {
+      "CompanyName":"Not yet",
+      
+    },
+    
+  ],
+  "skillsDescription":"Your skills here",
+  "skills":[
+    {
+      "skillname":"HTML5"
+    },
+    {
+      "skillname":"CSS"
+    },
+    {
+      "skillname":"Reactjs"
+    }
+  ],
+  "portfolio":[
+    {
+      "name":"project1",
+      "description":"Christmas Card",
+      "imgurl":"images/portfolio/cmc.png"
+    },
+    {
+      "name":"project2",
+      "description":"Countries Cards",
+      "imgurl":"images/portfolio/cc.png"
+    },
+    {
+      "name":"project3",
+      "description":"mMusic information Page",
+      "imgurl":"images/portfolio/music.png"
+    },
+    
+  ],
+  "testimonials":[
+    {
+      "description":"This is a sample testimonial",
+      "name":"Some technical guy"
+    },
+    
+  ]
+}
+
+export default resumeData
